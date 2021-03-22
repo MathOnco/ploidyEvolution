07555efe02075227795257f6429c1edba300ef81
Gregory Kimmel <gregory.kimmel89@gmail.com>
2021-03-22 11:58:01 -0400

    Added driver and install

---
+++ Julia/driver.jl
@@ -0,0 +1,162 @@
+using TOML, DelimitedFiles, ArgParse, JSON
+
+import Base.@kwdef
+
+# Used to search through command line for particular arguments
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table! s begin
+        "--input","-i"
+            help = "Specifies an inputfile to replace default parameters"
+        "--verbosity", "-v"
+            help = "Prints information to command line for debugging"
+            action = :store_true
+		"--cnFile", "-c"
+			help = "Contains numeric array of copy numbers"
+			default = "myX.txt"
+		"--birthRateFile", "-b"
+			help = "Contains numeric vector of birth rates"
+			default = "myY.txt"
+		"--u0"
+			help = "Array that contains CN of initial condition"
+			default = "[1,1,1,1,1]"
+    end
+	
+    return parse_args(s)
+end
+
+# Struct containing input data needed for simulation with default values
+@kwdef struct Input
+
+	debugging::Bool = false				# prints info from ploidyMovement
+	stepsize::Real = 1.0				# discretization of chromosome
+	minChrom::Real = 1.0				# minimum chromosome allowed
+	maxChrom::Real = 5.0				# maximum chromosome allowed
+	deathRate::Float64 = 0.1			# universal death rate
+	misRate::Float64 = 0.15				# universal missegregation rate
+	finalDay::Real = 30.0				# end of simulation
+	# CNmatFilename::String = "myX.txt"	# file containing CNs
+	# birthFilename::String = "myY.txt"	# file with birthrates
+	outputFile::String = "testing.csv"	# name of file for [CN soln(t)]	
+
+end
+
+function Input(inputFile::String)
+
+	# Dictionary that contains info from the toml (INPUT) file
+	data = TOML.tryparsefile(inputFile)
+
+	# Get the parameters for the struct.
+	debugging=get(data,"debugging",false)
+	stepsize=get(data,"stepsize",1.0)
+	minChrom=get(data,"minChrom",1.0)
+	maxChrom=get(data,"maxChrom",5.0)
+	deathRate=get(data,"deathRate",0.1)
+	misRate=get(data,"misRate",0.15)
+	finalDay=get(data,"finalDay",30.0)
+	# CNmatFilename=get(data,"CNmatFilename","myX.txt")
+	# birthFilename=get(data,"birthFilename","myY.txt")
+	outputFile=get(data,"outputFile","testing.csv")
+
+	# Avoid overwriting an old .csv file if using default
+	count = 1
+	while isfile(outputFile)
+		outputParent, outputExtension = split(outputFile,".")
+		outputFile = outputParent * "_" * string(count) * "." * outputExtension
+		count += 1
+	end
+
+
+	Input(
+		debugging,
+		stepsize,
+		minChrom,
+		maxChrom,
+		deathRate,
+		misRate,
+		finalDay,
+		# CNmatFilename,
+		# birthFilename,
+		outputFile
+		)
+
+end
+
+function initialize()
+
+	# Default values to be fed into ploidyMovement
+	data = Input()
+
+	# Grab command line args
+	parsed_args = parse_commandline()
+
+	# Replaces default parameters if input file is given
+	if !isnothing(parsed_args["input"])
+		data = Input(parsed_args["input"])
+	end
+
+	# Prints info if set to true
+	verbosity = parsed_args["verbosity"]
+
+	# Grab file containing copy number variation
+	CNmatFilename = parsed_args["cnFile"]
+
+	# Grab file containing birth rates
+	birthFilename = parsed_args["birthRateFile"]
+
+	# Grab initial condition (it is a string so we parse and convert)
+	u0 = Int.(JSON.parse(parsed_args["u0"]))
+
+	# Error checking
+	if !isfile(CNmatFilename)
+		error("$CNmatFilename file cannot be found")
+	end
+	if !isfile(birthFilename)
+		error("$birthFilename file cannot be found")
+	end
+
+	# Check to see if input file is given
+    if verbosity
+		println("parsed_args:")
+		for (arg,val) in parsed_args
+			println("  $arg => $val")
+		end
+	end
+
+	return data, CNmatFilename, birthFilename, u0, verbosity
+end
+
+function main()
+
+	# Grab input parameters and whether to print info to terminal
+	data, CNmatFilename, birthFilename, u0, verbosity = initialize()
+
+	if verbosity
+		println("Input:")
+		for name in fieldnames(typeof(data)) 
+			println("  $name => $(getfield(data,name))")
+		end
+	end
+
+	# Read the birth rate file to be used for the interpolation
+	X = readdlm(CNmatFilename, '\t')
+	Y = readdlm(birthFilename, '\t')
+
+	# FIXME!!!!!!! At the moment we assume ploidy is not included
+	X .+= 2.0 # adding ploidy by hand.
+
+	# Error if the elements in X or Y are not all subtypes of real
+	if any((eltype(X),eltype(Y)) .>: Real)
+		error("X and Y must contain only numeric values")
+	end
+
+	# readdlm gives a 2D array, we turn it into a vector (1d array) here.
+	Y = dropdims(Y,dims=2)
+
+	# Run ploidy movement
+	runPloidyMovement(data,X,Y,u0)
+	
+end
+
+main()
---
+++ Julia/ploidyMovement.jl
@@ -0,0 +1,184 @@
+using DifferentialEquations
+using LinearAlgebra
+using DelimitedFiles
+using Parameters
+
+# Load the file that contains the Polyharmonic interpolator function
+include("polyHarmonicInterp.jl")
+
+
+"""
+
+	plodyModel()
+
+	This function is used in DifferentialEquations and returns the RHS of the
+	differential equation needed to solve ploidy evolution of the discrete 
+	system.
+
+	INPUTS
+		du:		A vector of the RHS of y' = du
+		u:		A vector of the solution variable
+		p:		A list of parameters that we pass
+		t:		The current time point
+	OUTPUTS
+		du:		A vector of the RHS of y' = du
+
+
+"""
+function ploidyModel(du,u,pars,t)
+
+	# Grab the parameters
+	(interp,nChrom,chromArray,misRate,deathRate,debugging) = pars
+
+	# Iterate over each compartment
+	for (i,focal) in enumerate(
+		Iterators.product((1:length(j) for j in chromArray)...))
+
+		#=
+		1) calculateParents(focal, minChrom, maxChrom, stepChrom)
+		2) updatePopulation (a struct?)
+			sum(inflow(focal,parent,u[parent]) for parent in parentList)
+			- outflow(focal,u[focal])
+			a. inflow needs to calculate birth and flow rate
+			b. outflow needs to calculate birth, death and flow rate
+		=#
+
+		focalCN = collect(chromArray[k][focal[k]] for k in 1:nChrom)
+
+		parentCNList = calculateParents(focalCN, 1,5,1)
+
+		inflow = 0.0
+
+		# Inflow from the parents
+		for parentCN in parentCNList
+
+			# Get birth rate
+			birthRate = max(PolyharmonicInterpolation.polyharmonicSpline(interp,parentCN')[1],0.0)
+
+			# Get flow rate from parentCN -> focalCN
+			flowRate = q(parentCN,focalCN,misRate,nChrom)
+
+			# Will need to have this be the index in the future
+			inflow += birthRate*flowRate*u[Int.(parentCN)...] # *(1 - 1/avg(CN))
+
+		end
+
+		# Grab the focal cells birth rate
+		birthRate = max(PolyharmonicInterpolation.polyharmonicSpline(interp,focalCN')[1],0.0)
+
+		# Add it to the inflow to the focal compartment
+		inflow += birthRate*(1.0 - 2*misRate)*u[focal...]
+
+		# Death of the focal compartment
+		outflow = deathRate*u[focal...]
+
+		# Information if debugging
+		if debugging && (inflow != 0.0 || outflow != 0.0)
+			@show t,focalCN,inflow,outflow, u[focal...]
+		end
+
+		# Update the RHS
+		du[focal...] = inflow - outflow
+		
+	end
+
+end
+
+function calculateParents(offspring::Vector{T}, minChrom::Int,
+	maxChrom::Int,stepChrom::Int) where T<:Real
+
+
+	[ (x = copy(offspring); x[idx] = v; x)
+		for idx in 1 : length(offspring)
+			for v in max(offspring[idx]-1,minChrom):stepChrom:
+				min(offspring[idx]+1,maxChrom) if v != offspring[idx] ]
+
+end
+
+function q(parent::Vector{T},offspring::Vector{T},misRate::Float64,nChrom::Int) where T <: Real
+
+	# parentCN = collect(linspaces[k][parent[k]] for k in 1:nChrom)
+	# offspringCN = collect(linspaces[k][offspring[k]] for k in 1:nChrom)
+
+	parentCN = parent
+	offspringCN = offspring
+
+	CNdist = norm(parentCN .- offspringCN,1)
+
+	# No mutation occurred
+	if (CNdist == 0.0)
+		val = 1.0 - misRate
+	else
+		# FIXME: Should be a function dependent on parent ploidy
+		val = misRate/nChrom
+	end
+
+	return val
+
+end
+
+#############################################
+#											#
+#											#
+#					Main		 			#
+#											#
+#											#
+#############################################
+
+function runPloidyMovement(params,X::AbstractArray,Y::AbstractVector,
+	startingPopCN::AbstractArray)
+
+	# Grab the parameters from the struct
+	@unpack (debugging,
+	stepsize,
+	minChrom,
+	maxChrom,
+	deathRate,
+	misRate,
+	finalDay,
+	outputFile) = params
+
+	# Polyharmonic interpolator
+	interp = PolyharmonicInterpolation.PolyharmonicInterpolator(X,Y)
+
+	# Number of chromosomes (inferred from interp)
+	nChrom = interp.dim
+
+	# minimum and maximum size of copy number
+	minX,maxX = minimum(X,dims=1),maximum(X,dims=1)
+
+	# The allowable states
+	chromArray = [minChrom:stepsize:maxChrom for i in 1:nChrom]
+
+	# Total number of states
+	nComp = prod(length(arr) for arr in chromArray)
+
+	# Time interval to run simulation and initial condition
+	tspan = (0.0,finalDay)
+	u0 = zeros(Int(maxChrom)*ones(Int,nChrom)...)
+	u0[startingPopCN...] = 1.0
+
+	# run simulation
+	odePars = (interp,nChrom,chromArray,misRate,deathRate,debugging)
+	prob = ODEProblem(ploidyModel,u0,tspan,odePars)
+	sol = solve(prob,Tsit5(),maxiters=1e5,abstol=1e-8,reltol=1e-5,saveat=1)
+
+	# convert to array for output
+	soln = reshape(Array(sol),nComp,length(sol.t))
+
+	# Grab compartment -> CN array
+	cnArray = Array{Float64}(undef,nComp,nChrom)
+	for (i,focal) in enumerate(
+		Iterators.product((1:length(j) for j in chromArray)...))
+			cnArray[i,:] = collect(chromArray[k][focal[k]] for k in 1:nChrom)
+	end
+
+	# Concatnate the solution
+	output = hcat(cnArray,soln)
+
+	# save to file
+	writedlm( outputFile,  output, ',')
+
+	return
+
+end
