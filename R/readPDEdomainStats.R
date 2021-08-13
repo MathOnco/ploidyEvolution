6309069997d8eb3396a12183b791b98ee43e1501
noemiandor <noemi.andor@gmail.com>
2021-08-13 13:43:01 -0400

    label vessel boundaries, rectangular domain

--- R/readPDEdomainStats.R
+++ R/readPDEdomainStats.R
@@ -1,6 +1,6 @@
 devtools::source_url("https://github.com/noemiandor/Utils/blob/master/grpstats.R?raw=TRUE")
 library(matlab)
-Plot_ConcaveHull <- function(xx, yy,lcolor="black", alpha=0.4, add=T, level=0.5/length(xx)) {
+Plot_ConcaveHull <- function(xx, yy,lcolor="black", alpha=0.4, add=T, level=55) {
   library(MASS) 
   ##Remove outliers
   hQ=0.975; lQ=0.025
@@ -11,11 +11,12 @@ Plot_ConcaveHull <- function(xx, yy,lcolor="black", alpha=0.4, add=T, level=0.5/
   
   ##Contour
   dens2 <- kde2d(xx, yy, lims=c(min(xx)-sd(xx), max(xx)+sd(xx),   
-                                min(yy)-sd(yy), max(yy)+sd(yy)),n=55  )
+                                min(yy)-sd(yy), max(yy)+sd(yy)),n=level  )
   contour(dens2$x, dens2$y, dens2$z,col=lcolor, add=add, alpha=alpha); #,drawlabels=F,lwd=2 
   return(dens2)
 }
-
+minmax_x=c(200,840)
+minmax_y=c(200,850)
 OUTD="~/Projects/PMO/HighPloidy_DoubleEdgedSword/data/Konstantin_GBM/VesselCoordinates"
 ## Statistic to evaluate vessel thickness
 for(STAT in c("Nucleus: Area","Nucleus: Perimeter","Nucleus: Eosin OD mean","Nucleus: Hematoxylin OD mean")){
@@ -42,6 +43,12 @@ for(STAT in c("Nucleus: Area","Nucleus: Perimeter","Nucleus: Eosin OD mean","Nuc
       next;
     }
     cells_vessels = read.table(v,header = T,check.names = F,stringsAsFactors = F,sep="\t")
+    cells_vessels[,c("Centroid X µm","Centroid Y µm")]=apply(cells_vessels[,c("Centroid X µm","Centroid Y µm")],2,function(x) x-min(x))
+    # rect(-minmax_x[2],minmax_y[1],-minmax_x[1],minmax_y[2],col="purple",density = 0,lwd = 5)
+    cells_vessels = cells_vessels[cells_vessels$`Centroid X µm`>minmax_x[1] & 
+                                    cells_vessels$`Centroid X µm`<minmax_x[2] & 
+                                    cells_vessels$`Centroid Y µm`>minmax_y[1] & 
+                                    cells_vessels$`Centroid Y µm`<minmax_y[2],]
     plot(-cells_vessels$`Centroid X µm`,cells_vessels$`Centroid Y µm`,pch=20,main=fileparts(v)$name)
     vessels=cells_vessels[cells_vessels$Class=="Positive",]
     points(-vessels$`Centroid X µm`,vessels$`Centroid Y µm`,pch=20,col="red")
@@ -67,18 +74,24 @@ for(STAT in c("Nucleus: Area","Nucleus: Perimeter","Nucleus: Eosin OD mean","Nuc
     for(i in unique(vessels$VesselID)){
       ii=which(vessels$VesselID==i)
       onevessel=Plot_ConcaveHull(-vessels$`Centroid X µm`[ii],vessels$`Centroid Y µm`[ii],lcolor = i+1)
-      
-      xy=which(onevessel$z>quantile(onevessel$z,0.7), arr.ind=TRUE)
-      
+        
+      q=quantile(onevessel$z,(1:20)/20)
+      xy=which(onevessel$z>q["75%"], arr.ind=TRUE)
+      edge_xy=which(onevessel$z<=q["75%"] & onevessel$z>q["65%"], arr.ind=TRUE)
+        
       coord_=as.data.frame(t(apply(xy, 1, function(kl) c(onevessel$x[kl[1]],onevessel$y[kl[2]]))))
-      colnames(coord_)=c("Centroid X µm","Centroid Y µm")
+      edge_coord=as.data.frame(t(apply(edge_xy, 1, function(kl) c(onevessel$x[kl[1]],onevessel$y[kl[2]]))))
+      colnames(coord_) <- colnames(edge_coord) <-c("Centroid X µm","Centroid Y µm")
+      coord_$Boundary = 0
+      edge_coord$Boundary = 1
+      coord_ = rbind(coord_,edge_coord)
       coord_$VesselID=i
       # plot(coord_$`Centroid X µm`,coord_$`Centroid Y µm`,pch=20)
       coord[[as.character(i)]]=coord_
       points(-vessels$`Centroid X µm`[ii],vessels$`Centroid Y µm`[ii],pch=20,col=i+1)
     }
     allcoord=do.call(rbind,coord)
-    points(allcoord$`Centroid X µm`,allcoord$`Centroid Y µm`,pch=20,cex=0.4,col="brown")
+    points(allcoord$`Centroid X µm`,allcoord$`Centroid Y µm`,pch=20,cex=0.4,col=3+allcoord$Boundary)
     
     ## Remove noise
     vessels=vessels[vessels$VesselID!=0,]
@@ -88,13 +101,13 @@ for(STAT in c("Nucleus: Area","Nucleus: Perimeter","Nucleus: Eosin OD mean","Nuc
     vesselstats[,STAT]=vesselstats[,STAT]/max(vesselstats[,STAT])
     vesselstats[,STAT]=round(1+100*(vesselstats[,STAT]-min(vesselstats[,STAT])))
     vesselstats=vesselstats[sort(vesselstats[,STAT],index.return=T)$ix,]
-    col=rainbow(1.2*max(vesselstats[,STAT]))
+    col=1:(1.2*max(vesselstats[,STAT]))
     col=col[vesselstats[,STAT]]
     names(col)=as.character(vesselstats$VesselID)
     pdf(paste0(OUTD,filesep,fileparts(d_)$name,"_xy_",STAT,".pdf"),width = 10,height = 5)
     par(mfrow=c(1,2))
     plot(-cells_vessels$`Centroid X µm`,cells_vessels$`Centroid Y µm`,pch=20,main=fileparts(v)$name)
-    points(allcoord$`Centroid X µm`,allcoord$`Centroid Y µm`,pch=20,cex=0.4,col=col[as.character(allcoord$VesselID)])
+    points(allcoord$`Centroid X µm`,allcoord$`Centroid Y µm`,pch=20,cex=0.4,col=col[as.character(allcoord$VesselID)]+allcoord$Boundary)
     plot(1,main="vessel thickness",xaxt = "n",yaxt = "n",axes=F,xlab="",ylab="",col="white"); 
     legend("topleft",as.character(unique(vesselstats[,STAT])),fill=unique(col),cex=0.75)
     dev.off()
