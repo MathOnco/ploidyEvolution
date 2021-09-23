f53b79fe0b02a46a159641c4e6d4e0f714155647
gregorykimmel <gregory.kimmel89@gmail.com>
2021-09-23 10:16:09 -0400

    Overhauled toml file for readability.

--- Julia/driver.jl
+++ Julia/driver.jl
@@ -13,288 +13,256 @@ function parse_commandline()
 		"--spatial", "-s"
 			help = "Use the spatial ploidy model"
 			action = :store_true
-		# "--cnFile", "-c"
-		# 	help = "Contains numeric array of copy numbers and header of which chromosomes"
-		# 	default = "birthLandscapeBrainCancer.txt"
-		# "--birthRateFile", "-b"
-		# 	help = "Contains numeric vector of birth rates"
-		# 	default = "GrowthRate_brain_cancer_CLs.txt"
 		"--outputfile","-o"
 			help = "Specifies the filename for saving data from the simulation"
 			default = "output"
-		# "--u0"
-		# 	help = "Array that contains CN of initial condition"
-		# 	default = "[1,1,1,1,1]"
     end
 	
     return parse_args(s)
 end
 
-# Struct containing input data needed for simulation with default values
-struct WellMixedInput
+# Convert string keys to symbols (needed for splatting dictionary into struct fields)
+string_to_symbol(d::Dict) = Dict(Symbol(k) => v for (k,v) in d)
 
-	debugging::Int					# prints info from ploidyMovement
-	stepsize::Real					# discretization of chromosome
-	minChrom::Real					# minimum chromosome allowed
-	maxChrom::Real					# maximum chromosome allowed
-	deathRate::Float64				# universal death rate
-	misRate::Float64 				# universal missegregation rate
-	finalDay::Real					# end of simulation
-	u0::Vector{Int}					# Initial condition
-	replating::Bool					# Whether we replate the cells
-	startPop::Real					# Starting population size 
-	maxPop::Real					# Max population before replating
-	compartmentMinimum::Bool		# Sets sizes < 1 to 0 if true
-	copy_number_file::String		# Contains numeric array of copy numbers and header of which chromosomes
-	birth_rate_file::String			# Contains numeric vector of birth rates
-	progress_check::Bool			# Prints the current time of the simulation
-	interpolation_order::Int		# Allows the user to set the order of polyharmonic spline
+# Modified get function to get struct from the dict file
+function get_structs_from_file(d::Dict,key::String,default::DataType,verbosity::Bool)
+
+	local s
+	try
+		s = default(string_to_symbol(d[key]))
+		if verbosity
+			println("Replaced $key with inputfile")
+		end
+	catch
+		s = default()
+		if verbosity 
+			println("Did not replace $key with inputfile")
+		end
+	end
+
+	return s
 
 end
 
-struct SpatialInput
+struct Options
 
-	debugging::Int					# prints info from ploidyMovement
-	stepsize::Number				# discretization of chromosome
+	debugging::Int
+	compartmentMinimum::Bool
+	replating::Bool
+	maxPop::Number
+	progress_check::Bool
+	saveat::Number
+	interpolation_order::Int
+
+end
+
+struct Files
+	copy_number_file::String		# Contains numeric array of copy numbers and header of which chromosomes
+	birth_rate_file::String			# Contains numeric vector of birth rates
+	blood_vessel_file::String		# Blood vessel coordinate info
+end
+
+struct UniversalParameters
+	stepsize::Number
 	minChrom::Number
-	maxChrom::Number				# maximum chromosome allowed
-	deathRate::Number				# universal death rate
-	misRate::Float64				# universal missegregation rate
+	maxChrom::Number
+	deathRate::Number
+	misRate::Number
+	finalDay::Number
+	startPop::Number
+	starting_copy_number::Vector{Int}
+	max_cell_cycle_duration::Number
+end
+
+struct SpatialParameters
 	Γ::Number						# Random cell motion
 	Γₑ::Number						# Nutrient diffusion
 	ϕ::Number						# Nutrient level needed for half-maximal growth rate
 	Ξ::Number						# Energy needed for half maximal directional motion
 	χ::Number						# Directed motion magnitude
 	δ::Number						# Energy consumption
+	Np::Vector{Int}
 	k::Number
 	E_vessel::Number
-	saveat::Number
-	Np::Vector{Int}					# Grid size
-	finalDay::Real					# end of simulation
-	u0::Vector{Int}					# Initial condition
-	replating::Bool					# Whether we replate the cells
-	startPop::Real					# Starting population size 
-	maxPop::Real					# Max population before replating
-	compartmentMinimum::Bool		# Sets sizes < 1 to 0 if true
-	max_cell_cycle_duration::Number	# Length of cell cycle (assumed to be in hours)
-	copy_number_file::String		# Contains numeric array of copy numbers and header of which chromosomes
-	birth_rate_file::String			# Contains numeric vector of birth rates
-	blood_vessel_file::String		# Blood vessel coordinate info
-	progress_check::Bool			# Prints the current time of the simulation
-	interpolation_order::Int		# Allows the user to set the order of polyharmonic spline
-
-end
-
-function WellMixedInput()
-
-	# Get the parameters for the struct.
-	debugging= 0
-	stepsize= 1.0 
-	minChrom=1.0
-	maxChrom=5.0
-	deathRate=0.1
-	misRate=0.15
-	finalDay=30.0
-	u0 = [1,1,1,1,1]
-	replating=false
-	startPop=1e3
-	maxPop=1e6
-	compartmentMinimum=false
-	progress_check=false
-	interpolation_order=2
-	copy_number_file = "birthLandscapeBrainCancer.txt"
-	birth_rate_file = "GrowthRate_brain_cancer_CLs.txt"
-
-	WellMixedInput(
-		debugging,
-		stepsize,
-		minChrom,
-		maxChrom,
-		deathRate,
-		misRate,
-		finalDay,
-		u0,
-		replating,
-		startPop,
-		maxPop,
-		compartmentMinimum,
-		copy_number_file,
-		birth_rate_file,
-		progress_check,
-		interpolation_order
-		)
-
-end
-
-function WellMixedInput(inputFile::String)
-
-	# Dictionary that contains info from the toml (INPUT) file
-	data = TOML.tryparsefile(inputFile)
-
-	# Get the parameters for the struct.
-	debugging=get(data,"debugging",0)
-	stepsize=get(data,"stepsize",1.0)
-	minChrom=1.0
-	maxChrom=get(data,"maxChrom",5.0)
-	deathRate=get(data,"deathRate",0.1)
-	misRate=get(data,"misRate",0.15)
-	finalDay=get(data,"finalDay",30.0)
-	replating=get(data,"replating",false)
-	startPop=get(data,"startPop",1e3)
-	maxPop=get(data,"maxPop",1e6)
-	u0 = get(data,"u0",[1,1,1,1,1])
-	compartmentMinimum=get(data,"compartmentMinimum",false)
-	copy_number_file = get(data,"copy_number_file","birthLandscapeBrainCancer.txt")
-	birth_rate_file = get(data,"birth_rate_file","GrowthRate_brain_cancer_CLs.txt")
-	progress_check=get(data,"progress_check",false)
-	interpolation_order = get(data,"interpolation_order",2)
 	
+end
+
+# Struct containing input data needed for simulation with default values
+struct WellMixedInput
+
+	Options
+	Files
+	UniversalParameters
+
+end
+
+struct SpatialInput
+
+	Options
+	Files
+	UniversalParameters
+	SpatialParameters
+
+end
+
+struct WellMixedInputParameters
+
+	Options
+	UniversalParameters
+
+end
+
+struct SpatialInputParameters
+
+	Options
+	UniversalParameters
+	SpatialParameters
+	blood_vessel_file::String
+
+end
+
+function Files(;
+	copy_number_file ="birthLandscapeBrainCancer.txt",
+	birth_rate_file = "GrowthRate_brain_cancer_CLs.txt",
+	blood_vessel_file ="13496_2_Slides_and_Data_xy_test.txt")
+
+	Files(
+		copy_number_file,
+		birth_rate_file,
+		blood_vessel_file
+		)
+end
+
+Files(d::Dict) = Files(;d...)
+
+function UniversalParameters(;stepsize= 1.0, 
+	minChrom=1.0,
+	maxChrom=5.0,
+	deathRate=0.1,
+	misRate=0.15,
+	finalDay=30.0,
+	startPop=1e3,
+	starting_copy_number = [1,1,1,1,1],
+	max_cell_cycle_duration = 100)
+
+	UniversalParameters(
+					stepsize,
+					minChrom,
+					maxChrom,
+					deathRate,
+					misRate,
+					finalDay,
+					startPop,
+					starting_copy_number,
+					max_cell_cycle_duration
+					)
+
+end
+
+UniversalParameters(d::Dict) = UniversalParameters(;d...)
+
+function Options(;debugging = 3,
+	compartmentMinimum = false,
+	replating = true,
+	maxPop = 1e6,
+	progress_check = true,
+	saveat = 0.1,
+	interpolation_order = 2)
+
+	Options(
+			debugging,
+			compartmentMinimum,
+			replating,
+			maxPop,
+			progress_check,
+			saveat,
+			interpolation_order,
+			)
+
+end
+
+Options(d::Dict) = Options(;d...)
+
+
+
+function SpatialParameters(;
+	Γ = 0.1,
+	Γₑ = 1.0,
+	ϕ = 0.02,
+	Ξ = 0.02,
+	χ = 0.5,
+	δ = 0.05,
+	Np = [21,21],
+	k = 0.5,
+	E_vessel = 1.0)
+
+	SpatialParameters(
+					Γ,
+					Γₑ,	
+					ϕ,
+					Ξ,
+					χ,
+					δ,
+					Np,
+					k,
+					E_vessel
+					)
+
+
+
+end
+
+SpatialParameters(d::Dict) = SpatialParameters(;d...)
+
+function WellMixedInput(;Options=Options(),
+	Files=Files(),
+	UniversalParameters=UniversalParameters())
 
 	WellMixedInput(
-		debugging,
-		stepsize,
-		minChrom,
-		maxChrom,
-		deathRate,
-		misRate,
-		finalDay,
-		u0,
-		replating,
-		startPop,
-		maxPop,
-		compartmentMinimum,
-		copy_number_file,
-		birth_rate_file,
-		progress_check,
-		interpolation_order
-		)
+					Options,
+					Files,
+					UniversalParameters
+					)
 
 end
 
-function SpatialInput()
+function WellMixedInput(inputfile::String,verbosity::Bool=false)
 
-	# Get the parameters for the struct.
-	debugging= 0
-	stepsize= 1.0 
-	minChrom=1.0
-	maxChrom=5.0
-	deathRate=0.1
-	misRate=0.15
-	finalDay=30.0
-	u0 = [1,1,1,1,1]
-	replating=false
-	startPop=1e3
-	maxPop=1e6
-	compartmentMinimum=false
-	Γ = 0.05
-	Γₑ = 0.5
-	ϕ = 0.5
-	Ξ = 0.5
-	χ = 2.0
-	δ = 0.005
-	k,E_vessel,saveat = 0.5,1.0,0.1
-	Np = [21,21]
-	progress_check=false
-	interpolation_order=2
-	copy_number_file = "birthLandscapeBrainCancer.txt"
-	birth_rate_file = "GrowthRate_brain_cancer_CLs.txt"
-	blood_vessel_file = "13496_2_Slides_and_Data_xy_test.txt"
-	max_cell_cycle_duration = 100.0
+	data = TOML.tryparsefile(inputfile)
 
-	SpatialInput(
-		debugging,
-		stepsize,
-		minChrom,
-		maxChrom,
-		deathRate,
-		misRate,
-		Γ,
-		Γₑ,
-		ϕ,
-		Ξ,
-		χ,
-		δ,
-		k,E_vessel,saveat,
-		Np,
-		finalDay,
-		u0,
-		replating,
-		startPop,
-		maxPop,
-		compartmentMinimum,
-		max_cell_cycle_duration,
-		copy_number_file,
-		birth_rate_file,
-		blood_vessel_file,
-		progress_check,
-		interpolation_order
-		)
+	WellMixedInput(
+					get_structs_from_file(data,"Options",Options,verbosity),
+					get_structs_from_file(data,"Files",Files,verbosity),
+					get_structs_from_file(data,"UniversalParameters",UniversalParameters,verbosity)
+					)
+
 
 end
 
-function SpatialInput(inputFile::String)
-
-	# Dictionary that contains info from the toml (INPUT) file
-	data = TOML.tryparsefile(inputFile)
-
-	# Get the parameters for the struct.
-	debugging=get(data,"debugging",0)
-	stepsize=get(data,"stepsize",1.0)
-	minChrom = 1.0
-	maxChrom=get(data,"maxChrom",5.0)
-	deathRate=get(data,"deathRate",0.1)
-	misRate=get(data,"misRate",0.15)
-	finalDay=get(data,"finalDay",30.0)
-	replating=get(data,"replating",false)
-	startPop=get(data,"startPop",1e3)
-	maxPop=get(data,"maxPop",1e6)
-	compartmentMinimum=get(data,"compartmentMinimum",false)
-	Γ = get(data,"Γ",0.05)
-	Γₑ = get(data,"Γₑ",0.05)
-	ϕ = get(data,"ϕ",1.0)
-	Ξ = get(data,"Ξ",1.0)
-	χ = get(data,"χ",2.0)
-	δ = get(data,"δ",0.005)
-	k = get(data,"k",0.5)
-	E_vessel = get(data,"E_vessel",1.0)
-	saveat = get(data,"saveat",0.1)
-	Np = get(data,"Np",[21,21])
-	u0 = get(data,"u0",[1,1,1,1,1])
-	progress_check = get(data,"progress_check",false)
-	interpolation_order = get(data,"interpolation_order",2)
-	max_cell_cycle_duration = get(data,"max_cell_cycle_duration",100.0)
-	blood_vessel_file = get(data,"blood_vessel_file","13496_2_Slides_and_Data_xy_test.txt")
-	copy_number_file = get(data,"copy_number_file","birthLandscapeBrainCancer.txt")
-	birth_rate_file = get(data,"birth_rate_file","GrowthRate_brain_cancer_CLs.txt")
+function SpatialInput(;Options=Options(),
+	Files=Files(),
+	UniversalParameters=UniversalParameters(),
+	SpatialParameters=SpatialParameters())
 
 	SpatialInput(
-		debugging,
-		stepsize,
-		minChrom,
-		maxChrom,
-		deathRate,
-		misRate,
-		Γ,
-		Γₑ,
-		ϕ,
-		Ξ,
-		χ,
-		δ,
-		k,E_vessel,saveat,
-		Np,
-		finalDay,
-		u0,
-		replating,
-		startPop,
-		maxPop,
-		compartmentMinimum,
-		max_cell_cycle_duration,
-		copy_number_file,
-		birth_rate_file,
-		blood_vessel_file,
-		progress_check,
-		interpolation_order
-		)
+					Options,
+					Files,
+					UniversalParameters,
+					SpatialParameters
+					)
+end
+
+function SpatialInput(inputfile::String,verbosity::Bool=false)
+
+	data = TOML.tryparsefile(inputfile)
+
+	SpatialInput(
+					get_structs_from_file(data,"Options",Options,verbosity),
+					get_structs_from_file(data,"Files",Files,verbosity),
+					get_structs_from_file(data,"UniversalParameters",UniversalParameters,verbosity),
+					get_structs_from_file(data,"SpatialParameters",SpatialParameters,verbosity)
+					)
+
 
 end
 
@@ -312,20 +280,20 @@ function initialize()
 	outputFile = parsed_args["outputfile"]
 
 	# # Grab initial condition (it is a string so we parse and convert)
-	# u0 = Int.(JSON.parse(parsed_args["u0"]))
+	# starting_copy_number = Int.(JSON.parse(parsed_args["starting_copy_number"]))
 
 	# Default values to be fed into ploidyMovement
 	if spatial
 		input = SpatialInput()
 		# Replaces default parameters if input file is given
 		if !isnothing(parsed_args["input"])
-			input = SpatialInput(parsed_args["input"])
+			input = SpatialInput(parsed_args["input"],verbosity)
 		end
 	else
 		input = WellMixedInput()
 		# Replaces default parameters if input file is given
 		if !isnothing(parsed_args["input"])
-			input = WellMixedInput(parsed_args["input"])
+			input = WellMixedInput(parsed_args["input"],verbosity)
 		end
 	end
 
@@ -335,6 +303,7 @@ function initialize()
 		for (arg,val) in parsed_args
 			println("  $arg => $val")
 		end
+		println()
 	end
 
 	return input, outputFile, verbosity, spatial
@@ -373,22 +342,25 @@ function main()
 
 	# Printing stuff to terminal
 	if verbosity
-		println("Input:")
-		for name in fieldnames(typeof(input)) 
-			println("  $name => $(getfield(input,name))")
+		for name in fieldnames(typeof(input))
+			println("$(name):")
+			for var in fieldnames(typeof(getfield(input,name)))
+				println("  $var => $(getfield(getfield(input,name),var))")
+			end
+			println()
 		end
 	end
 
 	# Error checking
-	if !isfile(input.copy_number_file)
-		error("$(input.copy_number_file) file cannot be found")
+	if !isfile(input.Files.copy_number_file)
+		error("$(input.Files.copy_number_file) file cannot be found")
 	end
-	if !isfile(input.birth_rate_file)
-		error("$(input.birth_rate_file) file cannot be found")
+	if !isfile(input.Files.birth_rate_file)
+		error("$(input.Files.birth_rate_file) file cannot be found")
 	end
 
 	# Extract copy number (X) and birth rate (Y) to be used in the interpolation in ploidyMovement.jl
-	CN_birthrate_df = extract_XY(input.copy_number_file,input.birth_rate_file)
+	CN_birthrate_df = extract_XY(input.Files.copy_number_file,input.Files.birth_rate_file)
 
 	# Get copy number and birth rates
 	local copy_number,birth_rate,chromosome_header
@@ -404,11 +376,15 @@ function main()
 
 	# Run ploidy movement and grab params from the input
 	### FIX ME: We want to replace input file with dictionaries that we can grab the param chunk without deleting
-	params = input
 
 	if spatial
 
-		sol, cnArray = runPloidyMovement(params,copy_number,birth_rate,input.u0,spatial)
+		params = SpatialInputParameters(input.Options,
+										input.UniversalParameters,
+										input.SpatialParameters,
+										input.Files.blood_vessel_file)
+
+		sol, cnArray = runPloidyMovement(params,copy_number,birth_rate,spatial)
 
 		# Write results to multiple files by time points
 		for (index,t) in enumerate(sol.t)
@@ -426,12 +402,14 @@ function main()
 		end
 
 		# Save discretized domain
-		writedlm(outputFile*"_x.csv",range(0,1,length=params.Np[1]))
-		writedlm(outputFile*"_y.csv",range(0,1,length=params.Np[2]))
+		writedlm(outputFile*"_x.csv",range(0,1,length=params.SpatialParameters.Np[1]))
+		writedlm(outputFile*"_y.csv",range(0,1,length=params.SpatialParameters.Np[2]))
 
 	else
 
-		sol, cnArray,time = runPloidyMovement(params,copy_number,birth_rate,input.u0,spatial)
+		params = WellMixedInputParameters(input.Options,input.UniversalParameters)
+
+		sol, cnArray,time = runPloidyMovement(params,copy_number,birth_rate,spatial)
 
 		# convert to array for output
 		results = hcat(cnArray,sol)
